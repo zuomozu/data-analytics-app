1d210a26ec4b483fcf706088bea7f41dcba5c198
Manjot Singh <151872505+Manjotk07@users.noreply.github.com>
2024-07-25 16:27:49 -0400

    Update analysis.py

--- src/analysis.py
+++ src/analysis.py
@@ -1,1 +1,12 @@
+from flask import Flask, render_template
+import analysis
 
+app = Flask(_name_)
+
+@app.route('/')
+def index():
+    results = analysis.run_analysis()
+    return render_template('index.html', results=results)
+
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=5000)
