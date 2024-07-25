1a7f67d94b63306485faf5a1ea987a7f5c7b4932
Zuhair Moinuddin <49607944+zuomozu@users.noreply.github.com>
2024-07-25 16:37:52 -0400

    Update analysis.py

--- src/analysis.py
+++ src/analysis.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def run_analysis():
-    data = pd.read_csv('data/sample.csv')
+    data = pd.read_csv('./data/sample.csv')
     mean_value = data['value'].mean()
     return {'mean': mean_value}
